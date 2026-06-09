b0534cc0350b3077e9bcd8de4b1c15ca953a6e8b
Ali <alikhalibekov2006@gmail.com>
2026-06-10 01:57:05 +0300

    View для списка проектов. Создание и подключение маршрутов

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Project
 
-# Create your views here.
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'projects/project_list.html' # Путь к шаблону
+    context_object_name = 'projects'
+    paginate_by = 12
+
+    def get_queryset(self):
+        return Project.objects.all()
